1a99c3a6292269818c386c725501e62c5267eaa2
Brandon Shoop <brandon.shoop@gmail.com>
2026-03-10 14:51:34 -0400

    chore: remove dist/, fix .gitignore

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const node_server_1 = require("@hono/node-server");
-const server_1 = require("./server");
-const PORT = parseInt(process.env.PORT ?? "3000", 10);
-(0, node_server_1.serve)({ fetch: (0, server_1.createApp)().fetch, port: PORT }, () => console.log(`Critic listening on :${PORT}`));
-//# sourceMappingURL=index.js.map
